3be985428fe7dce1fc61863dd4aebe2c53ede702
Jere Wilvers <jwilversocampo@gmail.com>
2025-05-09 20:58:08 -0300

    Realizacion del TP Listas

---
+++ 13 Listas/TP Listas - WILVERS OCAMPO, Jeremias.py
@@ -0,0 +1,52 @@
+#Ejercicio 1
+lista=list(range(0,101,4))
+print(lista)
+
+#Ejercicio 2
+lista=["papafrita", "choripan", "sanguchito de miga", "coca cola light", "tia paola"]
+print(lista[-2])
+
+#Ejercicio 3
+lista_vacia = []
+lista_vacia.append ("Hola")
+lista_vacia.append ("mundo")
+lista_vacia.append ("Como estas?")
+print(lista_vacia)
+
+#Ejercicio 4
+animales = ["perro", "gato", "conejo", "pez"]
+animales[1] = "loro"
+animales[-1] = "oso"
+print (animales)
+
+#Ejercicio 5
+#En esete ejericio se busca remover con el codigo .remove el numero mas grande de una lista
+
+#Ejercicio 6
+lista= list(range(0,31,5))
+print(lista[0:2])
+
+#Ejercicio 7
+autos= ["sedan","polo","suran","gol"]
+autos[1]=["megan"]
+autos[2]=["ford"]
+print(autos)
+
+#Ejercicio 8
+dobles=[]
+dobles.append(2*5)
+dobles.append(2*10)
+dobles.append(2*15)
+print(dobles)
+
+
+#Ejercicio 9
+compras = [["pan", "leche"], ["arroz", "fideos", "salsa"],["agua"]]
+compras[2].append("jugo")
+compras[1][1]= ("tallarines")
+compras[0].remove("pan")
+print(compras)
+
+#Ejercicio 10
+listaAnidada=[15, True, [25.5, 57.9, 30.6], False]
+print(listaAnidada)
