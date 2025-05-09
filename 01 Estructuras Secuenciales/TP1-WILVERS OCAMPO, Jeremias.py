1aaa5c929d480d9f100db13571a30caff7b4db8d
Jere Wilvers <jwilversocampo@gmail.com>
2025-05-09 20:07:12 -0300

    Realizacion del TP 1

---
+++ 01 Estructuras Secuenciales/TP1-WILVERS OCAMPO, Jeremias.py
@@ -0,0 +1,11 @@
+#EJERCICIO 1
+print("Hola Mundo!")
+#EJERCICIO 2
+nombre = input("Por favor, ingresa tu nombre:")
+print(f"Hola" {nombre})
+#EJERCICO 3
+nombre = input("Por favor, ingresa tu nombre:")
+apellido = input("Por favor, ingresa tu apellido:")
+edad = input("Por favor, ingresa tu edad:")
+residencia = input("Por favor, ingresa tu lugar de resindencia:")
+print(f"Soy"+nombre+ +apellido+ "tengo" +edad+ "años y vivo en" +residencia) 
